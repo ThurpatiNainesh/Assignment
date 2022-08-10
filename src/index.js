c57acdd041950aa703efdc4e4bb1ec74c4d44ba2
ThurpatiNainesh <104067195+ThurpatiNainesh@users.noreply.github.com>
2022-08-10 18:57:14 +0530

    authorization

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongodb+s
+mongoose.connect("rv://ThurpatiNainesh:nain@cluster0.4uuwz0s.mongodb.net/test", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
